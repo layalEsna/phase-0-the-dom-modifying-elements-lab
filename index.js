da95f47faa0c58c5fbcd8700865e61694bf9af50
Amene Esnaashari <layla_1380@yahoo.com>
2024-01-14 08:57:42 -0600

    adding element to the node

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "Amene is the champion"
+document.body.append(newHeader)
+
